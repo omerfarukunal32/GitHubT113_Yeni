d0f78b33c59586aa564b5926c850df84bd5169b5
Ömer Ünal <mrfrknl3232@gmail.com>
2023-03-27 22:47:03 +0200

    270323

--- src/gitHub_IlkDers.java
+++ src/gitHub_IlkDers.java
@@ -1,5 +1,7 @@
 public class gitHub_IlkDers {
     public static void main(String[] args) {
         System.out.println("Hello Github");
+        System.out.println("====================");
+        System.out.println("have a nice day" );
     }
 }
