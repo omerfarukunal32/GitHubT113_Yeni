2db02df1b3d4196439e2cb508ec9c1885640d44a
Ömer Ünal <mrfrknl3232@gmail.com>
2023-03-30 00:49:17 +0200

    branchKullanimiAciklama

---
+++ src/BranchKullanimi.java
@@ -0,0 +1,16 @@
+public class BranchKullanimi {
+    /*
+    Branckh kullanımı
+        1- Bir proje üzerinde çalışırken yapılacak olan çalışmaların proejeyi
+        ya da son kullanıcıyo etkilememesi için farklı
+        alanlarda çalışmaların devam ettirilmesini sağlar
+        2-Branch genelde projede düzeltme/geliştirme/değişiklik yapma durumunda kullanılır
+        3-En önemli amaç çalışan projeyi riske atmamaktır.
+        4-Orjinalde çalışmak yerine  branchte çalışılır.
+        5-çalışma bitince Team Lead tarafından masterde merge edilir.
+        6- Çakışma durumunda ise Team Lead kontrol ederek karar verir
+            - Çakışmaların her ikisini de silebilir
+            - Birini silip birini tutabilir
+            - Her ikisini de çakışmayacak şekilde düzenleyip tutabilir
+     */
+}
