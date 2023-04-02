6cb20c35b9320610f361537702bb8a445326e61d
Ömer Ünal <mrfrknl3232@gmail.com>
2023-04-02 13:35:08 +0200

    4.Gun

--- src/G02_gitHub_IlkDers.java
+++ src/G02_gitHub_IlkDers.java
@@ -1,5 +1,23 @@
 public class G02_gitHub_IlkDers {
     public static void main(String[] args) {
         System.out.println("merhabe gitHub");
+
+        /*
+            Git-GitHub`da projelerimizin uzerinde calisirken temelde 3 adimda islem yapiyoruz
+                1. Projelerimizi/Dosyalarimizi git ile iliskilendiriyoruz
+                        git init
+
+                2. Projemizde / dosyamizda bulunanlari versiyon olusturuyorduk
+                        git add .
+                        git commit -m"versiyonAdi"
+
+                3. gitHub'a yollama/entegrasyonu
+                        git remote add origin repoUrlAdresi
+                        already origin  (hata verirse)
+                        git remote sel-url repoUrlAdresi
+
+                        git push
+         */
+
     }
 }
